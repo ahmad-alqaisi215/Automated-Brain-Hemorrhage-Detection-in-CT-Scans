1997eb1b9e80c44190030ffa70d15013995b2bfb
Ahmad Alqaisi <amq4319@gmail.com>
2025-05-21 02:34:43 +0300

    wrap sidebar navigation buttons inside  context

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,20 +1,21 @@
 import streamlit as st
 
 def render_sidebar():
-    st.markdown("# Navigation")
+    with st.sidebar:
+        st.markdown("# Navigation")
 
-    if st.button(" Home"):
-        st.session_state.page_selector = "Home"
-        st.rerun()
-    if st.button(" DCM Viewer"):
-        st.session_state.page_selector = "DCM Viewer"
-        st.rerun()
-    if st.button(" Diagnosis"):
-        st.session_state.page_selector = "Diagnosis"
-        st.rerun()
-    if st.button(" Report"):
-        st.session_state.page_selector = "Report"
-        st.rerun()
-    if st.button(" About"):
-        st.session_state.page_selector = "About"
-        st.rerun()
+        if st.button(" Home"):
+            st.session_state.page_selector = "Home"
+            st.rerun()
+        if st.button(" DCM Viewer"):
+            st.session_state.page_selector = "DCM Viewer"
+            st.rerun()
+        if st.button(" Diagnosis"):
+            st.session_state.page_selector = "Diagnosis"
+            st.rerun()
+        if st.button(" Report"):
+            st.session_state.page_selector = "Report"
+            st.rerun()
+        if st.button(" About"):
+            st.session_state.page_selector = "About"
+            st.rerun()
