c58e614cf935e94db13bcd041564c1002723ab56
Sarah Almashagbeh <sarah.almashagbehh@gmail.com>
2025-05-21 23:28:22 +0300

    uploaded the brain-model.glb 3Dmodel and edits on brain_anatomy file

--- app_pages/brain_anatomy.py
+++ app_pages/brain_anatomy.py
@@ -1,4 +1,22 @@
 import streamlit as st
+import streamlit.components.v1 as components
+from pathlib import Path
 
 def show():
     st.title("🧠 Brain Anatomy")
+    model_path = Path("C:/Users/SarahAl2203.SARAHALLAPTOP/Desktop/Automated-Brain-Hemorrhage-Detection-in-CT-Scans/human-brain.glb").as_posix()
+
+# Display the 3D model using <model-viewer>
+    components.html(f"""
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+
+    <model-viewer
+        src="file:///{model_path}"
+        alt="3D brain model"
+        auto-rotate
+        camera-controls
+        style="width: 100%; height: 500px; background-color: black; border-radius: 10px;"
+        exposure="1"
+        shadow-intensity="1">
+    </model-viewer>
+""", height=520)
