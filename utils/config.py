a5a1613b7aa0350b851e5cac1d5544e41347070b
Ahmad Alqaisi <amq4319@gmail.com>
2025-05-21 18:04:25 +0300

    config: define mean/std normalization values and temp upload directories

--- utils/config.py
+++ utils/config.py
@@ -1,5 +1,14 @@
+import os
+
+
 PAGE_CONFIG = dict (
     page_title="ICH Detection Assistant", 
     page_icon="🧠", 
     layout="wide"
     )
+
+MEAN_IMG = [0.22363983, 0.18190407, 0.2523437]
+STD_IMG = [0.32451536, 0.2956294,  0.31335256]
+ORIG_IMG_SIZE = 512
+TMP_DIR = 'tmp'
+UPLOAD_DIR = os.path.join(TMP_DIR, 'uploads')
