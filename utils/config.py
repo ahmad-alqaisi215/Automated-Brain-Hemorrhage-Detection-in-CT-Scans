1db13f6bd4b157a2402902fff1d30ea8e281624c
Ahmad Alqaisi <amq4319@gmail.com>
2025-05-22 16:59:52 +0300

    feat(config): add FEATURE_EXTRACTOR_PTH, N_CLASSES, and DEVICE setup

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,5 @@
 import os
+import torch
 
 
 PAGE_CONFIG = dict (
@@ -10,6 +11,14 @@ PAGE_CONFIG = dict (
 MEAN_IMG = [0.22363983, 0.18190407, 0.2523437]
 STD_IMG = [0.32451536, 0.2956294,  0.31335256]
 ORIG_IMG_SIZE = 512
+
 TMP_DIR = 'tmp'
+MODELS_DIR = 'models'
 UPLOAD_DIR = os.path.join(TMP_DIR, 'uploads')
 IMG_DIR = os.path.join(TMP_DIR, 'image')
+FEATURE_EXTRACTOR_PTH = os.path.join(MODELS_DIR, 'resnext101_32x8d_wsl_checkpoint.pth')
+
+N_CLASSES = 6
+
+DEVICE = torch.device('cuda')
+N_GPU = torch.cuda.device_count()
