fde4e35b9cad99a1ed1b650e6d800861ce915ee6
Ahmad Alqaisi <amq4319@gmail.com>
2025-05-22 17:04:40 +0300

    feat(preprocessing): add autocrop function to remove black edges from images

--- utils/data_pre_proc.py
+++ utils/data_pre_proc.py
@@ -114,3 +114,27 @@ def convert_img_to_base64(img):
     img_bytes = buffered.getvalue()
     base64_img = base64.b64encode(img_bytes).decode()
     return base64_img
+
+def autocrop(image, threshold=0):
+    """Crops any edges below or equal to threshold
+    Crops blank image to 1x1.
+    Returns cropped image.
+    https://stackoverflow.com/questions/13538748/crop-black-edges-with-opencv
+    """
+
+    if len(image.shape) == 3:
+        flatImage = np.max(image, 2)
+    else:
+        flatImage = image
+
+    rows = np.where(np.max(flatImage, 0) > threshold)[0]
+    cols = np.where(np.max(flatImage, 1) > threshold)[0]
+
+    image = image[cols[0]: cols[-1] + 1, rows[0]: rows[-1] + 1]
+
+    sqside = max(image.shape)
+
+    imageout = np.zeros((sqside, sqside, 3), dtype = 'uint8')
+    imageout[:image.shape[0], :image.shape[1],:] = image.copy()
+
+    return imageout
