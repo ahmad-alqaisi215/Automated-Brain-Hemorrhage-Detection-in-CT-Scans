026250b5d45f45237e7c3d0183ab10840cae9444
Sarah Almashagbeh <sarah.almashagbehh@gmail.com>
2025-05-24 19:02:04 +0300

    updated brain_anatomy and sidebar

--- components/sidebar.py
+++ components/sidebar.py
@@ -1,7 +1,25 @@
 import streamlit as st
+import streamlit.components.v1 as components
+from pathlib import Path
 
 def render_sidebar():
     with st.sidebar:
+        glb_url = 'https://raw.githubusercontent.com/AMQ4/Automated-Brain-Hemorrhage-Detection-in-CT-Scans/main/human-brain.glb'
+        
+        components.html(f"""
+    <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
+
+    <model-viewer
+        src="{glb_url}"
+        alt="3D brain model"
+        auto-rotate
+        camera-controls
+        style="width: 100%; height: 200px; background: transparent; border-radius: 10px;"
+        exposure="1"
+        shadow-intensity="1">
+    </model-viewer>
+""", height=200)
+        
         st.markdown("<div class='navbar'>", unsafe_allow_html=True)
         st.markdown("# Navigation")
 
